750228d940a2a0c71fee3ab429e60140a2087e52
Gary McLean Hall <gary@jaskwa.com>
2024-12-17 15:25:16 +0000

    Fixed relative paths to modules for networking foundation module

--- foundations/networking-foundation/main.tf
+++ foundations/networking-foundation/main.tf
@@ -1,10 +1,10 @@
 
 module project-context {
-  source = "../project-context/reader"
+  source = "../../modules/project-context/reader"
 }
 
 module dns-root {
-  source = "../dns/root"
+  source = "../../modules/dns_copy/root"
 
   tldp1 = module.project-context.value.tldp1
 }
