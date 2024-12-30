27481912d166e6cf0004499dba975a860c821b62
Gary McLean Hall <gary@jaskwa.com>
2024-12-30 14:53:24 +0000

    Foundations need `tf validate`, too

--- example/main.tf
+++ example/main.tf
@@ -1,7 +1,6 @@
 
 provider "aws" {
-  alias = "default"
-}
+  }
 
 provider "aws" {
   alias  = "virginia"
@@ -9,38 +8,39 @@ provider "aws" {
 }
 
 module "petstore_app" {
-  source = "../modules/domain-service"
+  source = "../modules/root/domain-service"
+
+  role = ""
 
   providers = {
-    aws      = aws.default
     aws.tls  = aws.virginia
     aws.edge = aws.virginia
   }
 
-  fqdn                    = "app.jfdi.jaskwa.com"
-  protected               = false
-  disaster_recovery_level = "bronze"
+  # fqdn                    = "app.jfdi.jaskwa.com"
+  # protected               = false
+  # disaster_recovery_level = "bronze"
 
-  statics = {
-    app = {
-      path = "*"
-      src  = "./src/static/"
-    }
-  }
+  # statics = {
+  #   app = {
+  #     path = "*"
+  #     src  = "./src/static/"
+  #   }
+  # }
 
-  apis = {
-    petstore = {
-      # type = "rest"
-      path = "*"
-      src  = "./src/api/"
-      spec = "petstore_spec.yaml"
-    }
-  }
+  # apis = {
+  #   petstore = {
+  #     # type = "rest"
+  #     path = "*"
+  #     src  = "./src/api/"
+  #     spec = "petstore_spec.yaml"
+  #   }
+  # }
 
-  datastores = {
-    pets = {
-      type = "rdbms"
-      src  = "./src/db"
-    }
-  }
+  # datastores = {
+  #   pets = {
+  #     type = "rdbms"
+  #     src  = "./src/db"
+  #   }
+  # }
 }
