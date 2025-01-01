f8555ceeff55b27d35afb1815cdbaf5a6c7f00ab
Gary McLean Hall <gary@jaskwa.com>
2025-01-01 14:56:21 +0000

    `tf fmt`

--- modules/account-lookup/main.tf
+++ modules/account-lookup/main.tf
@@ -16,7 +16,7 @@ data "aws_organizations_organization" "org" {
 
 locals {
   non_master_accounts = data.aws_organizations_organization.org.non_master_accounts
-  
+
   # This should come from `org`?
   account_markers = [
     "security",
@@ -28,23 +28,23 @@ locals {
   ]
 
   marker_accounts = {
-    for marker in local.account_markers:
-    marker => [ 
-      for nma in local.non_master_accounts: 
-      nma 
-      if strcontains(nma.name, marker) 
+    for marker in local.account_markers :
+    marker => [
+      for nma in local.non_master_accounts :
+      nma
+      if strcontains(nma.name, marker)
     ]
   }
 
   accounts = {
-    for marker, accounts in local.marker_accounts:
+    for marker, accounts in local.marker_accounts :
     marker => length(accounts) <= 1 ? one(accounts) : null
   }
 }
 
 check "exactly_one_account_for_every_marker" {
   assert {
-    condition = alltrue([ for marker, accounts in local.accounts: contains(local.account_markers, marker) && accounts != null ])
+    condition     = alltrue([for marker, accounts in local.accounts : contains(local.account_markers, marker) && accounts != null])
     error_message = "'Account not found' or 'too many accounts found' for marker"
   }
 }
--- modules/account-lookup/outputs.tf
+++ modules/account-lookup/outputs.tf
@@ -7,7 +7,7 @@ output "tooling" {
   value = local.accounts["tooling"]
 
   precondition {
-    condition = contains(keys(local.accounts), "tooling") && local.accounts["tooling"] != null
+    condition     = contains(keys(local.accounts), "tooling") && local.accounts["tooling"] != null
     error_message = "No `tooling` account found..."
   }
 }
@@ -16,16 +16,16 @@ output "networking" {
   value = local.accounts["networking"]
 
   precondition {
-    condition = contains(keys(local.accounts), "networking") && local.accounts["networking"] != null
+    condition     = contains(keys(local.accounts), "networking") && local.accounts["networking"] != null
     error_message = "No `networking` account found..."
   }
 }
 
 output "security" {
   value = local.accounts["security"]
-  
+
   precondition {
-    condition = contains(keys(local.accounts), "security") && local.accounts["security"] != null
+    condition     = contains(keys(local.accounts), "security") && local.accounts["security"] != null
     error_message = "No `security` account found..."
   }
 }
@@ -34,7 +34,7 @@ output "development" {
   value = local.accounts["development"]
 
   precondition {
-    condition = contains(keys(local.accounts), "development") && local.accounts["development"] != null
+    condition     = contains(keys(local.accounts), "development") && local.accounts["development"] != null
     error_message = "No `development` account found..."
   }
 }
@@ -43,7 +43,7 @@ output "staging" {
   value = local.accounts["staging"]
 
   precondition {
-    condition = contains(keys(local.accounts), "staging") && local.accounts["staging"] != null
+    condition     = contains(keys(local.accounts), "staging") && local.accounts["staging"] != null
     error_message = "No `staging` account found..."
   }
 }
@@ -52,7 +52,7 @@ output "production" {
   value = local.accounts["production"]
 
   precondition {
-    condition = contains(keys(local.accounts), "production") && local.accounts["production"] != null
+    condition     = contains(keys(local.accounts), "production") && local.accounts["production"] != null
     error_message = "No `production` account found..."
   }
 }
